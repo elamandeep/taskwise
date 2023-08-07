bcc5c3b98d99f4469e1a673e08e9a56491bf808b
4D46 <119421025+Mani4D46@users.noreply.github.com>
2023-08-07 12:12:15 +0100

    remade: ui

--- app/taskwise/ui.py
+++ app/taskwise/ui.py
@@ -6,13 +6,20 @@ User interface used in the app
 import sys
 from typing import List
 
-import rich.console
+import colorama
 import rich
+
+SELECT_MARK = "[yellow]>"
+QUESTION_MARK = "[blue]?[/]"
+ERROR_MARK = "[red]✕[/]"
+EXCLAMATION_MARK = '[magenta]![/]'
+CHECK_MARK = '[green]✓[/]'
+EXIT_MARK = '[bright_black]✕[/]'
 
 TASKS = [['c1', False, 't1'], ['c1', True, 't2'], ['c1', True, 't3'],
          ['c2', True, 't1'], ['c2', False, 't2']]
+
 CLOSE_MESSAGE = "Operation cancelled."
-RICH_COSOLE = rich.console.Console()
 
 
 try:
@@ -25,26 +32,72 @@ try:
 
         return chr(int.from_bytes(msvcrt.getch(), "little"))
 except ImportError:
+    import termios
+
     def getch():
         """
         Gets a single character from STDIO.
         """
-        import tty
-        import termios
-        file_number = sys.stdin.fileno()
-        old = termios.tcgetattr(file_number)
+        file_no = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(file_no)
+        term = termios.tcgetattr(file_no)
         try:
-            tty.setraw(file_number)
-            return sys.stdin.read(1)
+            term[3] &= ~(termios.ICANON | termios.ECHO | termios.IGNBRK |
+                         termios.BRKINT)
+            termios.tcsetattr(file_no, termios.TCSAFLUSH, term)
+
+            character = sys.stdin.read(1)
         finally:
-            termios.tcsetattr(file_number, termios.TCSADRAIN, old)
+            termios.tcsetattr(file_no, termios.TCSADRAIN, old_settings)
+        return character
+
+
+IS_OS_WINDOWS = sys.platform in ("win32", "cygwin")
+
+
+def getkey() -> str:
+    """
+    Returns the key pressed by user. if None it waits untill a key is pressed
+    """
+    if IS_OS_WINDOWS is False:
+        character1 = getch()
+
+        if character1 != "\x1B":
+            return character1
+
+        character2 = getch()
+        if character2 not in "\x4F\x5B":
+            return character1 + character2
+
+        character3 = getch()
+        if character3 not in "\x31\x32\x33\x35\x36":
+            return character1 + character2 + character3
+
+        character4 = getch()
+        if character4 not in "\x30\x31\x33\x34\x35\x37\x38\x39":
+            return character1 + character2 + character3 + character4
+
+        character5 = getch()
+        return character1 + character2 + character3 + character4 + character5
+    else:
+        character = getch()
+
+        if character not in "\x00\xe0":
+            return character
+
+        character2 = getch()
+
+        return "\x00" + character2
+
 
 if sys.platform.startswith(("linux", "darwin", "freebsd")):
     UP = "\x1b\x5b\x41"
     DOWN = "\x1b\x5b\x42"
 elif sys.platform in ("win32", "cygwin"):
-    UP = "\xe0\x48"
-    DOWN = "\xe0\x50"
+    UP = "\x00\x48"
+    DOWN = "\x00\x50"
+DELETE = "\x12"
+EXIT = "\x03"
 
 stdout = sys.stdout
 
@@ -60,52 +113,45 @@ def option_selector(options: List[str], message: str) -> int:
     """
     selected_option = 0
 
-    rich.print(f'[blue]?[/] {message}: [bright_black]» - Use arrow-keys.'
-               + ' Return to submit.', flush=False)
+    rich.print(f'{QUESTION_MARK} {message}: [bright_black]» - Use'
+               + ' arrow-keys. Return to submit.', flush=False)
 
     while True:
         for option_id, _option in enumerate(options):
             if option_id == selected_option:
-                rich.print(f'>   [u]{_option}', flush=False, end='')
+                rich.print(f'{SELECT_MARK}   {_option}', flush=False, end='')
             else:
                 rich.print(f'    {_option}', flush=False, end='')
             stdout.write("\n")
         stdout.flush()
 
-        if sys.platform.startswith(("linux", "darwin", "freebsd")):
-            character = getch()
-            if character[-1] in "\x4F\x5B":
-                character += getch()
-            if character[-1] in "\x31\x32\x33\x35\x36":
-                character += getch()
-        elif sys.platform in ("win32", "cygwin"):
-            character = getch()
-            if character in '\x00\xe0':
-                character += getch()
+        key = getkey()
 
-        if character in {UP, DOWN}:
+        if key in {UP, DOWN}:
             stdout.write("\033[1A\033[2K" * len(options))
-            if character == DOWN:
+            if key == DOWN:
                 if selected_option < len(options) - 1:
                     selected_option += 1
                 else:
                     selected_option = 0
-            elif character == UP:
+            elif key == UP:
                 if selected_option > 0:
                     selected_option -= 1
                 else:
                     selected_option = len(options) - 1
         else:
-            stdout.write("\033[1A\033[2K" * (len(options) + 1))
+            stdout.write("\033[1A\033[2K" * (len(options)))
             stdout.flush()
 
-            if character in '\r\n':
+            if key in '\r\n':
+                stdout.write("\033[1A\033[2K")
                 return selected_option
-            else:
-                rich.print(f'[red]❌[/] {message}:'
+            elif key == EXIT:
+                stdout.write("\033[1A\033[2K")
+                rich.print(f'{ERROR_MARK} {message}:'
                            + f' [bright_black]» {options[selected_option]}.',
                            flush=False)
-                rich.print(f'[bright_black]❌[/] {CLOSE_MESSAGE}',
+                rich.print(f'{EXIT_MARK} {CLOSE_MESSAGE}',
                            flush=False)
                 stdout.flush()
                 return
@@ -115,66 +161,71 @@ def display_tasks(tasks, category):
     """
     Returns based on given filters
     """
-    rich.print('[magenta]![/] [bright_black]» Use arrow-keys.'
-               + ' Return to toggle checkmark.', flush=False)
+    rich.print(f'{EXCLAMATION_MARK} [bright_black]» Use arrow-keys.'
+               + ' Return to toggle checkmark. Ctrl + R to delete'
+               + ' selected task permanently.', flush=False)
 
     selected_task = 0
     tasks += [[category, None, '[blue]+']]
 
     while True:
         for task_id, task in enumerate(tasks):
+            if task not in TASKS:
+                task[0] = category
+                TASKS.append(task)
             if task[1] is True:
-                task = f'✔️ {task[2]}'
+                task = f'{CHECK_MARK} {task[2]}'
             else:
                 task = f'  {task[2]}'
             if task_id == selected_task:
-                rich.print(f'> [u]{task}', flush=False, end='')
+                rich.print(f'{SELECT_MARK} {task}', flush=False, end='')
             else:
                 rich.print(f'  {task}', flush=False, end='')
             stdout.write("\n")
         stdout.flush()
 
-        if sys.platform.startswith(("linux", "darwin", "freebsd")):
-            character = getch()
-            if character[-1] in "\x4F\x5B":
-                character += getch()
-            if character[-1] in "\x31\x32\x33\x35\x36":
-                character += getch()
-        elif sys.platform in ("win32", "cygwin"):
-            character = getch()
-            if character in '\x00\xe0':
-                character += getch()
+        key = getkey()
 
-        if character in {UP, DOWN}:
+        if key in {UP, DOWN, DELETE}:
             stdout.write("\033[1A\033[2K" * len(tasks))
-            if character == DOWN:
+            if key == DOWN:
                 if selected_task < len(tasks) - 1:
                     selected_task += 1
                 else:
                     selected_task = 0
-            elif character == UP:
+            elif key == UP:
                 if selected_task > 0:
                     selected_task -= 1
                 else:
                     selected_task = len(tasks) - 1
+            elif key == DELETE:
+                if tasks[selected_task][1] is not None:
+                    TASKS.pop(TASKS.index(tasks[selected_task]))
+                    tasks.pop(selected_task)
         else:
             stdout.write("\033[1A\033[2K" * len(tasks))
             stdout.flush()
-            if character in '\r\n' and tasks[selected_task][1] is None:
+            if key in '\r\n' and tasks[selected_task][1] is None:
                 if tasks[selected_task][2][-1] == '+':
-                    name = RICH_COSOLE.input('[blue]?[/] Choose an name for'
-                                             + ' this task: [bright_black]» ')
+                    rich.print(f'{QUESTION_MARK} Choose an name'
+                               + ' for this task: [bright_black]» ',
+                               flush=False, end='')
+                    name = input(colorama.Fore.LIGHTBLACK_EX)
                     stdout.write("\033[1A\033[2K")
-                    TASKS.insert(0, [category, False, name])
-            elif character in '\r\n':
+                    if name.strip() != '' and (
+                            name.strip() not in [task[2] for task in tasks]
+                    ):
+                        tasks.insert(-1, [category, False, name])
+            elif key in '\r\n':
                 TASKS[TASKS.index(tasks[selected_task])][1] = not (
                     TASKS[TASKS.index(tasks[selected_task])][1]
                 )
-            else:
+                tasks[selected_task][0] = category
+            elif key == EXIT:
                 stdout.write("\033[1A\033[2K")
-                rich.print('[red]❌[/][bright_black] Use arrow-keys.'
+                rich.print(f'{ERROR_MARK}[bright_black] Use arrow-keys.'
                            + ' Return to toggle checkmark.', flush=False)
-                rich.print(f'[bright_black]❌[/] {CLOSE_MESSAGE}',
+                rich.print(f'{EXIT_MARK} {CLOSE_MESSAGE}',
                            flush=False)
                 stdout.flush()
                 return
@@ -194,7 +245,8 @@ def display_task_categories():
     categories_names = list(categories.keys())
     category = option_selector(categories_names, 'Select a category')
     if category is not None:
-        display_tasks(categories[categories_names[category]], category)
+        display_tasks(categories[categories_names[category]],
+                      categories_names[category])
 
 
 option = option_selector(['Display Tasks', 'exit'], 'Select an option')
